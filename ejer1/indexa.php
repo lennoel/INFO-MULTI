b29d880deb154de8989bb47b68801a58346f1ba7
Leonel Vijay Beltrán Montes <44306634+lennoel@users.noreply.github.com>
2024-05-06 02:14:09 -0400

    Add files via upload

---
+++ ejer1/indexa.php
@@ -0,0 +1,421 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Banco Grinshow</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="css2/style.css">
+</head>
+
+<body data-spy="scroll" data-target="#navegacion-principal" data-offset="10">
+
+    <nav id="navegacion-principal" class="navbar fixed-top navbar-light bg-transparent">
+        <div class="container">
+            <a class="navbar-brand text-light" href="#">Banco Grinshow <span>2024</span></a>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link" href="#APA1">Quienes Somos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#APA2">Cuenta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#APA3">Transación</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="embed-responsive embed-responsive-16by9">
+        <img class="embed-responsive-item " src="video/concierto.jpg">
+
+        <div class="container">
+            <div class="informacion-concierto text-light">
+                <h1>Banco Central Grinshow<i class="fa-solid fa-cat"></i></h1>
+                <p class="p-3 text-dark bg-success d-inline-block mt-5 rounded"> El Banco de todos los Bolivianos y
+                    Gatos </p>
+            </div>
+        </div>
+    </div>
+
+    <main class="contenido-principal mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="img/imagen1.jpg" class="img-fluid">
+                </div>
+                <div class="col-md-6 mt-5 mt-md-0 sobre-festival align-items-center d-flex">
+                    <div class="contenido">
+                        <h2 class="bg-light text-dark">Nadie compara nuestras ofertas</h2>
+                        <p class="text-dark bg-light d-inline-block">Oferta Especial Solo Por Este Mes! del 01 abril al
+                            30 abril de 2024 </p>
+
+                        <p>¡Banco Central Grinshow te ofrece una oferta que no podrás rechazar!<br><br>
+
+                            ¿Necesitas dinero rápido? <br><br>
+                            ¡Nosotros te lo prestamos al increíblemente bajo interés del 1000%!<br><br>
+
+                            ¡Sí, has leído bien! Te prestamos dinero al 1000% de interés. <br><br>
+                            ¿Por qué conformarte con tasas de interés bajas cuando puedes disfrutar del emocionante y
+                            exorbitante 1000%?<br><br>
+                            ¡No pierdas esta oportunidad única en la vida! ¡Ven y solicita tu préstamo hoy mismo y
+                            empieza a vivir la vida al máximo endeudándote con nosotros!<br>
+
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <div id="APA1" class="APA1 bg-danger my-5 p-5">
+        <h1 class="text-center">Banco Central Grinshow<i class="fa-solid fa-cat"></i></h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+
+                    <ul class="horario list-unstyled mt-4  pt-1">
+                        <li class="p-3">
+                            <h4 class="text-light mt-4 text-center separador rock"></h4>
+                            <p class="font-weight-bold m-0">¡Descubre Quiénes Somos!<br>
+                                <span class="font-weight-normal">
+                                    En el Banco Central Grinshow, hacemos que el fraude sea divertido. Somos los maestros del engaño, los expertos en sacarte el dinero sin que te des cuenta.<br><br>
+                                    ¿Quieres un préstamo? Te lo damos, con tasas de interés tan altas que te dejarán sin aliento<br><br>
+                                    ¿Por qué conformarte con un banco aburrido cuando puedes unirte a la diversión del Banco Central Grinshow? ¡Ven y únete a la fiesta del fraude!
+                                </span>
+                            </p>
+                        </li>
+
+                    </ul>
+
+
+                </div>
+
+                <div class="col-md-6">
+                    <ul class="horario1 list-unstyled mt-4  pt-1">
+                        <li class="p-3">
+                            <p class="font-weight-bold m-0">¡Experimenta Nuestro Servicio de Atención al Cliente!<br>
+                                <span class="font-weight-normal">
+                                    En el Banco Central Grinshow, llevamos la atención al cliente a otro nivel. Somos expertos en hacerte sentir como en casa mientras te despojamos de tus preocupaciones financieras. ¿Necesitas ayuda? ¡Estamos aquí para confundirte y desviarte!<br><br>
+                                    ¿Estás cansado de la atención al cliente aburrida y sin emociones? ¡Ven y únete a nosotros para una experiencia que nunca olvidarás! Nuestros representantes están entrenados para ofrecerte respuestas evasivas y promesas vacías, todo mientras te mantienen atrapado en el ciclo interminable de llamadas en espera y correos electrónicos sin respuesta.
+                                </span>
+                            </p>
+                            <h4 class="text-light mt-4 text-center separador dj"></h4>
+                        </li>
+
+                    </ul>
+
+
+                </div>
+            </div>
+        </div><
+
+
+    </div>
+
+
+    <div id="APA2" class="container APA2">
+        <h1 class="text-center">Cuentas <i class="fa-solid fa-shield-cat"></i></h1>
+        <div class="text-center col mb-4 fs-5 ">
+            <h1>Mostrar Información de Persona</h1>
+            <form method="post" action="#APA2">
+                <label for="persona_id">Ingrese el ID de la persona:</label>
+                <input type="text" id="persona_id" name="persona_id" required>
+                <button type="submit" name="submit">Mostrar Cuenta</button>
+            </form>
+            <?php
+
+            include("conexBD.php");
+            if (isset($_POST['submit'])) {
+                
+                $persona_id = $_POST['persona_id'];
+
+                if ($conex) {
+                   
+                    $consulta = "SELECT Persona.ID_Persona, Persona.Nombre, Persona.Apellido, 
+                                CuentaBancaria.ID_Cuenta, CuentaBancaria.NumeroCuenta, CuentaBancaria.Saldo, 
+                                Transaccion.ID_Transaccion, Transaccion.Tipo, Transaccion.Monto, Transaccion.Fecha 
+                        FROM Persona 
+                        INNER JOIN CuentaBancaria ON Persona.ID_Persona = CuentaBancaria.ID_Persona
+                        INNER JOIN Transaccion ON CuentaBancaria.ID_Cuenta = Transaccion.ID_Cuenta
+                        WHERE Persona.ID_Persona = $persona_id";
+
+                    
+                    $resultado = mysqli_query($conex, $consulta);
+
+                    
+                    if ($resultado) {
+                        
+                        echo '<div class="text-center mb-4" ><table style="font-size: 18px;" class="table table-striped">';
+                        echo '<tr >
+                        <th>ID Persona</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>ID Cuenta</th>
+                        <th>Número de Cuenta</th>
+                        <th>Saldo</th>
+                        <th>ID Transacción</th>
+                        <th>Tipo</th>
+                        <th>Monto</th>
+                        <th>Fecha</th>
+                      </tr>';
+
+                       
+                        while ($row = $resultado->fetch_array()) {
+                            
+                            echo "<tr>
+                            <td>{$row['ID_Persona']}</td>
+                            <td>{$row['Nombre']}</td>
+                            <td>{$row['Apellido']}</td>
+                            <td>{$row['ID_Cuenta']}</td>
+                            <td>{$row['NumeroCuenta']}</td>
+                            <td>{$row['Saldo']}</td>
+                            <td>{$row['ID_Transaccion']}</td>
+                            <td>{$row['Tipo']}</td>
+                            <td>{$row['Monto']}</td>
+                            <td>{$row['Fecha']}</td>
+                          </tr>";
+                        }
+
+                        // Cierra la tabla
+                        echo '</table></div>';
+                    } else {
+                        echo "Error en la consulta: " . mysqli_error($conex);
+                    }
+                } else {
+                    echo "Error en la conexión a la base de datos.";
+                }
+            }
+            ?>
+        </div>
+    </div>
+
+
+
+    <div id="APA3" class="APA3 bg-danger py-5">
+        <h2 class="text-center text-dark mb-4">ABC</h2>
+
+        <div class="container">
+            <?php
+           
+            include("conexBD.php");
+
+            
+            function validarDatos($dato)
+            {
+                $dato = trim($dato);
+                $dato = stripslashes($dato);
+                $dato = htmlspecialchars($dato);
+                return $dato;
+            }
+
+            
+            if (isset($_POST['submit_persona'])) {
+               
+                $id_persona = validarDatos($_POST['id_persona']);
+                $nombre = validarDatos($_POST['nombre']);
+                $apellido = validarDatos($_POST['apellido']);
+
+              
+                $sql = "INSERT INTO Persona (ID_Persona, Nombre, Apellido) VALUES ('$id_persona', '$nombre', '$apellido')";
+                if (mysqli_query($conex, $sql)) {
+                    echo "Persona agregada correctamente.";
+                } else {
+                    echo "Error al agregar persona: " . mysqli_error($conex);
+                }
+            }
+
+           
+            if (isset($_POST['submit_cuenta'])) {
+                
+                $id_cuenta = validarDatos($_POST['id_cuenta']);
+                $id_persona = validarDatos($_POST['id_persona']);
+                $numero_cuenta = validarDatos($_POST['numero_cuenta']);
+                $saldo = validarDatos($_POST['saldo']);
+
+                
+                $sql = "INSERT INTO CuentaBancaria (ID_Cuenta,ID_Persona, NumeroCuenta, Saldo) VALUES ('$id_cuenta','$id_persona', '$numero_cuenta', '$saldo')";
+                if (mysqli_query($conex, $sql)) {
+                    echo "Persona agregada correctamente.";
+                } else {
+                    echo "Error al agregar persona: " . mysqli_error($conex);
+                }
+            }
+
+           
+            if (isset($_POST['submit_transaccion'])) {
+              
+                $id_transaccion = validarDatos($_POST['id_transaccion']);
+                $id_cuenta = validarDatos($_POST['id_cuenta']);
+                $tipo = validarDatos($_POST['tipo']);
+                $monto = validarDatos($_POST['monto']);
+                if (isset($_POST['fecha'])) {
+                    $fecha = validarDatos($_POST['fecha']);
+                } else {
+                 
+                    $fecha = "Valor predeterminado";
+                }
+
+               
+                $sql = "INSERT INTO Transaccion (ID_Transaccion,ID_Cuenta, Tipo, Monto,Fecha) VALUES ('$id_transaccion','$id_cuenta', '$tipo', '$monto','$fecha')";
+                if (mysqli_query($conex, $sql)) {
+                    echo "Transacción agregada correctamente.";
+                } else {
+                    echo "Error al agregar transacción: " . mysqli_error($conex);
+                }
+            }
+
+          
+            mysqli_close($conex);
+            ?>
+            <div id="contenedor">
+                <div id="agregarPersona" class="form__group field">
+                    <h2>Agregar Nueva Persona</h2>
+                    <form method="post">
+                        <input required="" type="number" id="id_persona" name="id_persona" autocomplete="off" class="form__field" placeholder="Name">
+                        <label class="form_label" for="id_persona">ID_Persona:</label>
+
+
+                        <input required="" type="text" id="nombre" name="nombre" autocomplete="off" class="form__field" placeholder="Name">
+                        <label class="form_label" for="nombre">Nombre:</label>
+
+
+                        <input type="text" id="apellido" name="apellido" autocomplete="off" class="form__field" placeholder="Name">
+                        <label class="form_label" for="apellido">Apellido:<br></label><br>
+                        <button type="submit" name="submit_persona" class="btn-1">Agregar Persona</button>
+                    </form>
+                </div>
+
+                <div id="agregarCuenta" class="form__group field">
+                    <h2>Agregar Nueva Cuenta Bancaria</h2>
+                    <form method="post">
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="number" id="id_cuenta" name="id_cuenta" required>
+                        <label class="form_label" for="id_cuenta">ID de Cuenta:</label>
+
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="number" id="id_persona" name="id_persona" required>
+                        <label class="form_label" for="id_persona">ID de Persona:</label>
+
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="text" id="numero_cuenta" name="numero_cuenta" required>
+                        <label class="form_label" for="numero_cuenta">Número de Cuenta:</label>
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="number" id="saldo" name="saldo" required><br>
+                        <label class="form_label" for="saldo">Saldo:</label>
+
+                        <button type="submit" name="submit_cuenta" class="btn-1">Agregar Cuenta Bancaria</button>
+                    </form>
+                </div>
+                <div id="agregarTransaccion" class="form__group field">
+                    <h2>Agregar Nueva Transacción</h2>
+                    <form method="post">
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="number" id="id_transaccion" name="id_transaccion" required>
+                        <label class="form_label" for="id_transaccion">ID de Transacion:</label>
+
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="number" id="id_cuenta" name="id_cuenta" required>
+                        <label class="form_label" for="id_cuenta">ID de Cuenta:</label>
+
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="text" id="tipo" name="tipo" required>
+                        <label class="form_label" for="tipo">Tipo de Transacción:</label>
+
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="number" id="monto" name="monto" required>
+                        <label class="form_label" for="monto">Monto:</label>
+
+                        <input required="" autocomplete="off" class="form__field" placeholder="Name" type="text" id="fecha" name="fecha" required>
+                        <label class="form_label" for="fecha">Fecha:</label>
+
+                        <button type="submit" name="submit_transaccion" class="btn-1">Agregar Transacción</button>
+                    </form>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+    </div>
+    <div>
+        <?php
+        
+        include("conexBD.php");
+
+       
+        if (!function_exists('validarDatos')) {
+            function validarDatos($dato)
+            {
+                $dato = trim($dato);
+                $dato = stripslashes($dato);
+                $dato = htmlspecialchars($dato);
+                return $dato;
+            }
+        }
+
+        
+        if (isset($_POST['submit_eliminar_persona'])) {
+           
+            $id_persona_eliminar = validarDatos($_POST['id_persona_eliminar']);
+
+
+            $sql_eliminar_transacciones = "DELETE FROM Transaccion 
+                                   WHERE ID_Cuenta IN (SELECT ID_Cuenta FROM CuentaBancaria WHERE ID_Persona = '$id_persona_eliminar')";
+
+           
+            $sql_eliminar_cuentas = "DELETE FROM CuentaBancaria WHERE ID_Persona = '$id_persona_eliminar'";
+
+            $sql_eliminar_persona = "DELETE FROM Persona WHERE ID_Persona = '$id_persona_eliminar'";
+
+          
+            mysqli_begin_transaction($conex);
+
+           
+            if (mysqli_query($conex, $sql_eliminar_transacciones)) {
+               
+                if (mysqli_query($conex, $sql_eliminar_cuentas)) {
+                    
+                    if (mysqli_query($conex, $sql_eliminar_persona)) {
+                       
+                        mysqli_commit($conex);
+                        echo "Persona y sus datos asociados eliminados correctamente.";
+                    } else {
+                       
+                        mysqli_rollback($conex);
+                        echo "Error al eliminar persona: " . mysqli_error($conex);
+                    }
+                } else {
+                    
+                    mysqli_rollback($conex);
+                    echo "Error al eliminar cuentas bancarias: " . mysqli_error($conex);
+                }
+            } else {
+               
+                mysqli_rollback($conex);
+                echo "Error al eliminar transacciones: " . mysqli_error($conex);
+            }
+        }
+
+       
+        mysqli_close($conex);
+        ?>
+        <div id="eliminarPersona" class="form__group field">
+    <h2>Eliminar Persona y Sus Datos Asociados</h2>
+    <form method="post">
+        <input required="" autocomplete="off" class="form__field" type="number" id="id_persona_eliminar" name="id_persona_eliminar" placeholder="ID de Persona a Eliminar">
+        <button type="submit" name="submit_eliminar_persona" class="btn-1">Eliminar Persona</button>
+    </form>
+</div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+
+</html>

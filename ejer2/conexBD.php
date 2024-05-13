140ddae85e92c9033af9e26305e21feb538da7f0
Leonel Vijay Beltrán Montes <44306634+lennoel@users.noreply.github.com>
2024-05-13 11:35:42 -0400

    Delete ejer2 directory

--- ejer2/conexBD.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conex = mysqli_connect("localhost", "ejemplo", "123456", "BDLeonel");
-?>
--- ejer2/setting.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define("USUARIO","sa");
-define("PASSWORD","root");
-define("DATABASE","BDLeonel");
-define("SERVIDOR","SQLEXPRESS");
-?>

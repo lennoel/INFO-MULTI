3c2970d8e13143dd37041b0ac62392727f8acfca
Leonel Vijay Beltrán Montes <44306634+lennoel@users.noreply.github.com>
2024-06-17 07:27:54 -0400

    Add files via upload

--- ejer1/indexa.php
+++ ejer1/indexa.php
@@ -1,52 +1,8 @@
-<!doctype html>
-<html lang="en">
+<?php
+include "cabeza.inc.php";
+?>
 
-<head>
-    <title>Banco Grinshow</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="css7/style.css">
-</head>
 
-<body data-spy="scroll" data-target="#navegacion-principal" data-offset="10">
-
-    <nav id="navegacion-principal" class="navbar fixed-top navbar-light bg-transparent">
-        <div class="container">
-            <a class="navbar-brand text-light" href="#">Banco Grinshow <span>2024</span></a>
-            <ul class="nav nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link" href="#APA1">Quienes Somos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#APA2">Cuenta</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#APA3">Agregar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#APA4">Eliminar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#APA5">Modificar</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <div class="embed-responsive embed-responsive-16by9">
-        <img class="embed-responsive-item " src="video/concierto.jpg">
-
-        <div class="container">
-            <div class="informacion-concierto text-light">
-                <h1>Banco Central Grinshow<i class="fa-solid fa-cat"></i></h1>
-                <p class="p-3 text-dark bg-success d-inline-block mt-5 rounded"> El Banco de todos los Bolivianos y
-                    Gatos </p>
-            </div>
-        </div>
-    </div>
 
     <main class="contenido-principal mt-5">
         <div class="container">
@@ -232,70 +188,6 @@
 
                 </div>
             </div>
-            <div id="APA5" class="APA5  py-5">
-                <div id="container">
-                    <h2 class="text-center mx-auto">Modificar Datos</h2>
-                    <?php
-                    include("modificar.php");
-                    ?>
-                    <div id="contenedor">
-                        <div id="modificarPersona" class="form__group field">
-                            <h2>Modificar Persona y Sus Datos Asociados</h2>
-                            <form method="post" action="#APA5">
-                                <input required="" autocomplete="off" class="form__field" type="number" id="id_persona_modificar" name="id_persona_modificar" placeholder="ID de Persona a Modificar">
-                                <label class="form_label">ID Persona</label>
-
-                                <input required="" autocomplete="off" class="form__field" type="text" id="nombre_modificar" name="nombre_modificar" placeholder="Nuevo Nombre">
-                                <label class="form_label">Nombre</label>
-
-                                <input required="" autocomplete="off" class="form__field" type="text" id="apellido_modificar" name="apellido_modificar" placeholder="Nuevo Apellido">
-                                <label class="form_label">Apellidos</label>
-                                <button type="submit" name="submit_modificar_persona" class="btn-1">Modificar Persona</button>
-                            </form>
-                        </div>
-                        <div id="modificarCuenta" class="form__group field">
-                            <h2>Modificar Cuenta Bancaria </h2>
-                            <form method="post" action="#APA5">
-                                <input required="" autocomplete="off" class="form__field" type="number" id="id_cuenta_modificar" name="id_cuenta_modificar" placeholder="ID de Cuenta a Modificar">
-                                <label class="form_label">ID Cuentra</label>
-                                <input required="" autocomplete="off" class="form__field" type="text" id="numero_cuenta_modificar" name="numero_cuenta_modificar" placeholder="Nuevo Número de Cuenta">
-                                <label class="form_label">Numero Cuenta</label>
-                                <input required="" autocomplete="off" class="form__field" type="number" id="saldo_modificar" name="saldo_modificar" placeholder="Nuevo Saldo">
-                                <label class="form_label">Saldo</label>
-                                <button type="submit" name="submit_modificar_cuenta" class="btn-1">Modificar Cuenta</button>
-                            </form>
-                        </div>
-                        <div id="modificarTransaccion" class="form__group field">
-                            <h2>Modificar Transacción</h2>
-                            <form method="post" action="#APA5">
-                                <input required="" autocomplete="off" class="form__field" type="number" id="id_transaccion_modificar" name="id_transaccion_modificar" placeholder="ID de Transacción a Modificar">
-                                <label class="form_label">ID Transaccion</label>
-                                <input required="" autocomplete="off" class="form__field" type="text" id="tipo_modificar" name="tipo_modificar" placeholder="Nuevo Tipo de Transacción">
-                                <label class="form_label">Tipo Transaccion</label>
-                                <input required="" autocomplete="off" class="form__field" type="number" id="monto_modificar" name="monto_modificar" placeholder="Nuevo Monto">
-                                <label class="form_label">Monto</label>
-                                <input required="" autocomplete="off" class="form__field" type="text" id="fecha_modificar" name="fecha_modificar" placeholder="Nueva Fecha">
-                                <label class="form_label">Fecha</label>
-                                <button type="submit" name="submit_modificar_transaccion" class="btn-1">Modificar Transacción</button>
-                            </form>
-                        </div>
-                    </div>
-
-
-
-                </div>
-
-
-
-            </div>
-
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-
-            <script src="js/jquery.countdown.min.js"></script>
-            <script src="js/app.js"></script>
-
-</body>
-
-</html>
+<?php
+include "cabeza.inc.php";
+?>
